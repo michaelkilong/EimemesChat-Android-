ece7db2bcb0b6adbe4631a2c575ff169171e7d1c
Michael Kilong <michaelkilongz@gmail.com>
2026-03-23 09:15:58 +0530

    Add files via upload

--- app/src/main/java/com/eimemes/chat/EimemesChatApp.kt
+++ app/src/main/java/com/eimemes/chat/EimemesChatApp.kt
@@ -1,1 +1,13 @@
+package com.eimemes.chat
 
+import android.app.Application
+import com.google.firebase.FirebaseApp
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EimemesChatApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
---
+++ app/src/main/java/com/eimemes/chat/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.eimemes.chat
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.eimemes.chat.ui.AppNavigation
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppNavigation()
+        }
+    }
+}
