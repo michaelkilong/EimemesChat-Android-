8d61ffe897072fa91d76c3c285ad23be34bf3e9b
Michael Kilong <michaelkilongz@gmail.com>
2026-03-23 13:04:19 +0530

    Update SplashScreen.kt

--- app/src/main/java/com/eimemes/chat/ui/SplashScreen.kt
+++ app/src/main/java/com/eimemes/chat/ui/SplashScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.eimemes.chat.ui.theme.AccentBlue
 import com.eimemes.chat.ui.theme.AccentPurple
