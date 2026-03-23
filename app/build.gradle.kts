02e170499f8ef50a9055ac237c431b551ffa43fc
Michael Kilong <michaelkilongz@gmail.com>
2026-03-23 11:18:35 +0530

    Update dependencies in build.gradle.kts
    Removed obsolete credential libraries and added play services auth.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,9 +68,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
 
-    implementation(libs.credentials)
-    implementation(libs.credentials.play.services)
-    implementation(libs.googleid)
+    implementation(libs.play.services.auth)
 
     implementation(libs.okhttp)
     implementation(libs.okhttp.sse)
